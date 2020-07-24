fccf2d7cda2084d70d7190f482636e110dee9e28
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-07-24 19:04:39 -0400

    load spinner example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import LoadSpinner from './LoadSpinner/LoadSpinner';
+
 
 function App() {
+  // alternatively to useState, you could make
+  // this a class and use state = {}, setState, etc
+  const [isLoaded, setIsLoaded] = useState();
+
+  const handleIsLoadedToggle = () => {
+    isLoaded ? setIsLoaded(false) : setIsLoaded(true)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleIsLoadedToggle}>Toggle LoadSpinner</button>
+      {isLoaded && <LoadSpinner />}
     </div>
   );
 }
